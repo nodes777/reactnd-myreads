8d28041f49ac58537c845c86594a004e886bbf9f
Taylor <taylor.nodell@gmail.com>
2019-01-17 18:55:47 -0500

    fixes undefined thumbnail bug

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import BookDetails from './BookDetails'
 
-
+const isUndefinedThumbnail = (url) => url === undefined;
 const BookList = ({ bookShelfTitle, bookList, updateBookShelf }) => (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{bookShelfTitle}</h2>
@@ -14,7 +14,10 @@ const BookList = ({ bookShelfTitle, bookList, updateBookShelf }) => (
                 id={book.id}
                 title={book.title}
                 authors={book.authors}
-                imageURL={book.imageLinks.thumbnail ? book.imageLinks.thumbnail : "http://via.placeholder.com/128x193?text=No%20Cover"}
+                imageURL={isUndefinedThumbnail(book.imageLinks) 
+                  ?  "http://via.placeholder.com/128x193?text=No%20Cover" 
+                  : book.imageLinks.thumbnail
+                  }
                 updateBookShelf={updateBookShelf}
                 book={book}
               />
